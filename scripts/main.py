fe164346304f80bd95dea1527aeecd2f428db396
Masabathula V S Raghavendra Rao <masabathularao@uptycs.com>
2023-11-27 11:41:45 +0530

    comment elk errors for s1

--- scripts/main.py
+++ scripts/main.py
@@ -175,9 +175,10 @@ if __name__ == "__main__":
 
         #--------------------------------Elk Erros------------------------------------------------
         elk_errors = None
-        print("Fetching Elk Errors ...")
-        elk = Elk_erros(start_timestamp=start_timestamp,end_timestamp=end_timestamp,prom_con_obj=prom_con_obj)
-        elk_errors = elk.fetch_errors()
+        if test_env_json_details["stack"]!="S1":
+            print("Fetching Elk Errors ...")
+            elk = Elk_erros(start_timestamp=start_timestamp,end_timestamp=end_timestamp,prom_con_obj=prom_con_obj)
+            elk_errors = elk.fetch_errors()
         
         #--------------------------------cpu and mem node-wise---------------------------------------
         print("Fetching resource usages data ...")
