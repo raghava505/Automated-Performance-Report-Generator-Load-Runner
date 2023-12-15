3b06465aa83e4ba878f1aad546cf5ae797f7fc5a
Madhusudhan Reddy <mreddy@uptycs.com>
2023-12-15 21:30:54 +0530

    fix config path issue

--- scripts/kubequery/selfmanaged_accuracy.py
+++ scripts/kubequery/selfmanaged_accuracy.py
@@ -9,14 +9,17 @@ from fabric import Connection
 from datetime import timedelta
 from pathlib import Path
 
-PROJECT_ROOT = Path(__file__).resolve().parent
-CONFIG_PATH = "./config"
+# Variables
+ROOT_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+base_stack_config_path = f"{ROOT_PATH}/config"
 
 class SelfManaged_Accuracy:
 
     def __init__(self,start_timestamp,end_timestamp,prom_con_obj,variables):
         
-        with open(os.path.join(CONFIG_PATH,variables["test_env_file_name"]),"r") as file:
+        test_env_file_path = "{}/{}".format(base_stack_config_path,variables["test_env_file_name"])
+        
+        with open(test_env_file_path,"r") as file:
             data = json.load(file)
             # print(data)
         
