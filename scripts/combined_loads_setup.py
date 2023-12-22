886c24c0a3f21e09366ca7cd9daa88fd4906e6af
tchamata-uptycs <tchamata@uptycs.com>
2023-12-22 12:30:17 +0530

    small changes

--- scripts/combined_loads_setup.py
+++ scripts/combined_loads_setup.py
@@ -9,8 +9,6 @@ class all_combined_child(parent):
                     "total_assets": "32K Control Plane +  8K Multi customer",
                     "records_sent_per_hour_per_customer": "51,84,000",
                     "records_sent_per_hour" : "51,84,00,000",
-                    "alert_rules_enabled_per_cust":12,
-                    "alert_rules_triggered_per_cust":12,
                     "assets_per_cust":80,
                     "input_file": "rhel7-6tab_12rec.log",
                     "events_table_name": "dns_lookup_events, socket_events, process_events, process_file_events"
@@ -26,8 +24,6 @@ class osquery_cloudquery_combined_child(parent):
                     "total_assets": "52k (12k Multicustomer + 40 k Control plane)",
                     "records_sent_per_hour_per_customer": "6,480,000",
                     "records_sent_per_hour" : "77,76,00,000",
-                    "alert_rules_enabled_per_cust":39,
-                    "alert_rules_triggered_per_cust":33,
                     "assets_per_cust":100,
                     "input_file": "rhel7-6tab_12rec.log",
                     "events_table_name": "dns_lookup_events, socket_events, process_events, process_file_events"
