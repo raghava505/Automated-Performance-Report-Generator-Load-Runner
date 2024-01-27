ad20ade72313c3268d1b69c10c8d48527026cf05
Masabathula V S Raghavendra Rao <masabathularao@uptycs.com>
2024-01-27 11:08:29 +0530

    change s1 stack file

--- scripts/osquery/osquery_child_class.py
+++ scripts/osquery/osquery_child_class.py
@@ -27,7 +27,7 @@ class osquery_child(parent):
             "ControlPlane":{
                 "total_number_of_customers": "1",
                 "test_title": "Single Customer Control Plane Load",
-                "total_assets": "200K",
+                "total_assets": "120K",
                 "redis_switchover_case_time_ist":""
             },
     }
