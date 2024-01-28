419bd3d68d6e1ecb9d234ac942906ec94ca14d5f
Masabathula V S Raghavendra Rao <masabathularao@uptycs.com>
2024-01-28 19:46:43 +0530

    updating active conn code

--- scripts/active_conn_by_apps.py
+++ scripts/active_conn_by_apps.py
@@ -1,13 +1,14 @@
-import requests
+from helper import execute_prometheus_query
 import pandas as pd
 
 class Active_conn:
-    def __init__(self,prom_con_obj,start_timestamp,end_timestamp):
-        self.curr_ist_start_time=start_timestamp
-        self.curr_ist_end_time=end_timestamp
+    def __init__(self,prom_con_obj,start_timestamp,end_timestamp,hours):
+        self.start_timestamp=start_timestamp
+        self.end_timestamp=end_timestamp
         self.prom_con_obj=prom_con_obj
         self.PROMETHEUS = self.prom_con_obj.prometheus_path
         self.API_PATH = self.prom_con_obj.prom_api_path
+        self.hours=hours
     
     def get_avg_active_conn(self):
         base_query="uptycs_pg_idle_active_connections_by_app{{state=\"active\",db=\"{}\",role=\"master\"}}"
@@ -15,26 +16,13 @@ class Active_conn:
         result_dict={}
         for db in dbs:
             query = base_query.format(db)
-            print(f"Prometheus query : {query}")
+            result =execute_prometheus_query(self.prom_con_obj,self.start_timestamp,self.end_timestamp,query,self.hours)
             final=[]
-            PARAMS = {
-                'query': query,
-                'start': self.curr_ist_start_time,
-                'end': self.curr_ist_end_time,
-                'step':60
-            }
-            response = requests.get(self.PROMETHEUS + self.API_PATH, params=PARAMS)
-            if response.status_code != 200:print("ERROR : Request failed")
-            result = response.json()['data']['result']
-            if len(result)==0:
-                print(f"WARNING : No data found for : {db}, the query executed is : {query}")
-                continue
             for app in result:
                 application_name = app["metric"]["application_name"]
-                values = [float(i[1]) for i in app['values']]
-                avg = sum(values) / len(values)
-                minimum = min(values)
-                maximum = max(values)
+                avg = app["values"]["average"]
+                minimum = app["values"]["minimum"]
+                maximum = app["values"]["maximum"]
                 final.append({"application":application_name,"minimum":minimum , "maximum":maximum,"average":avg})
             df = pd.DataFrame(final)
             new_row={"application":"TOTAL","minimum":df["minimum"].sum() , "maximum":df["maximum"].sum(),"average":df["average"].sum()}
@@ -52,8 +40,8 @@ class Active_conn:
 #     import pytz
 #     format_data = "%Y-%m-%d %H:%M"
     
-#     start_time_str = "2024-01-24 03:58"
-#     hours=12
+#     start_time_str = "2024-01-26 13:25"
+#     hours=4
 
 #     start_time = datetime.strptime(start_time_str, format_data)
 #     end_time = start_time + timedelta(hours=hours)
@@ -71,6 +59,6 @@ class Active_conn:
 #     end_timestamp = int(end_ist_time.timestamp())
 #     end_utc_time = end_ist_time.astimezone(utc_timezone)
 #     end_utc_str = end_utc_time.strftime(format_data)
-#     active_obj = Active_conn(configuration('longevity_nodes.json') , start_timestamp,end_timestamp)
+#     active_obj = Active_conn(configuration('longevity_nodes.json') , start_timestamp,end_timestamp,hours)
 #     result = active_obj.get_avg_active_conn()
 #     print(result)
