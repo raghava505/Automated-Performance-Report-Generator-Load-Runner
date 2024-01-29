189791354a1089c9522ffb381db7c48f3a22fd72
Masabathula V S Raghavendra Rao <masabathularao@uptycs.com>
2024-01-29 11:47:24 +0530

    updating trino queries fetching and kafka topics

--- scripts/trino_queries_analysis.py
+++ scripts/trino_queries_analysis.py
@@ -2,12 +2,13 @@ import json
 import paramiko
 import pandas as pd
 from io import StringIO
+from helper import execute_trino_query
 
 #configdb_command = "PGPASSWORD=pguptycs psql -h {} -U uptycs -p 5432 -d configdb -c \"select read_password from customer_database where database_name='upt_{}';\"".format(remote_host,self.stack_details['domain'])
 
 class TRINO_ANALYSE:
     def __init__(self,start_utc_str,end_utc_str,prom_con_obj):
-
+        self.prom_con_obj = prom_con_obj
         self.test_env_file_path=prom_con_obj.test_env_file_path
         with open(self.test_env_file_path, 'r') as file:
             self.stack_details = json.load(file)
@@ -18,40 +19,25 @@ class TRINO_ANALYSE:
         self.dnode = self.stack_details['dnodes'][0]
 
     def fetch_trino_results(self,commands):
-        save_dict={}
-        ssh_client = paramiko.SSHClient()
-        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        try:
-            ssh_client.connect(self.dnode, 22, self.remote_username, self.remote_password)
-            for heading,value in commands.items():
-                raw_command=value['query']
-                columns=value['columns']
-                command = raw_command.replace("<start_utc_str>",self.start_utc_str).replace( "<end_utc_str>", self.end_utc_str)
-                complete_command = f"sudo -u monkey TRINO_PASSWORD=prestossl /opt/uptycs/cloud/utilities/trino-cli --insecure --server https://localhost:5665 --schema upt_system --user uptycs --catalog uptycs --password --truststore-password sslpassphrase --truststore-path /opt/uptycs/etc/presto/presto.jks --execute \"{command}\""
-                print(f"Executing '{heading}' command in {self.dnode} : " )
-                print(complete_command)
-                stdin, stdout, stderr = ssh_client.exec_command(complete_command)
-                output = stdout.read().decode('utf-8')
-                errors = stderr.read().decode('utf-8')
-                if errors:
-                    print("Errors:")
-                    print(errors)
-                stringio = StringIO(output)
-                df = pd.read_csv(stringio, header=None, names=columns)
-                integer_columns = df.select_dtypes(include='int').columns
-                string_columns = df.select_dtypes(include='object').columns
-                print("Integer Columns:", list(integer_columns))
-                print("String Columns:", list(string_columns))
-                new_row=dict([(int_col,df[int_col].sum()) for int_col in integer_columns])
-                new_row.update(dict([(str_col,"TOTAL") for str_col in string_columns]))
-                df = df._append(new_row, ignore_index=True)
-                print(df)
-                save_dict[heading] = df.to_dict(orient='records')
+        save_dict={}       
+        for heading,value in commands.items():
+            raw_command=value['query']
+            columns=value['columns']
+            query = raw_command.replace("<start_utc_str>",self.start_utc_str).replace( "<end_utc_str>", self.end_utc_str)
+            print(f"Executing '{heading}' command in {self.dnode} : " )
+            output= execute_trino_query(self.dnode,query,self.prom_con_obj)
+            stringio = StringIO(output)
+            df = pd.read_csv(stringio, header=None, names=columns)
+            integer_columns = df.select_dtypes(include='int').columns
+            string_columns = df.select_dtypes(include='object').columns
+            print("Integer Columns:", list(integer_columns))
+            print("String Columns:", list(string_columns))
+            new_row=dict([(int_col,df[int_col].sum()) for int_col in integer_columns])
+            new_row.update(dict([(str_col,"TOTAL") for str_col in string_columns]))
+            df = df._append(new_row, ignore_index=True)
+            print(df)
+            save_dict[heading] = df.to_dict(orient='records')
 
-        except Exception as e:
-            print(f"Error: {str(e)}")
-        finally:
-            ssh_client.close()
         return save_dict
     
 # if __name__=='__main__':
