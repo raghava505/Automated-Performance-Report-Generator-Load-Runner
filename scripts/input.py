b43ea670f91132606ae9fe41b54fb06c531abf7e
Masabathula V S Raghavendra Rao <masabathularao@uptycs.com>
2024-02-09 18:24:27 +0530

    supporting float hours

--- scripts/input.py
+++ scripts/input.py
@@ -87,7 +87,8 @@ def create_input_form():
             input_index = int(input(f"Enter : {' '.join(str(key).split('_')).title()} {helper_text} : ").strip())
             input_value = bool_options[input_index]
         elif Type == int:
-            Type=float
+            if key == "load_duration_in_hrs":
+                Type=float
             input_value=float(input(f"Enter : {' '.join(str(key).split('_')).title()}  (example: {value}) : ").strip())
 
         details[key] = Type(input_value)
