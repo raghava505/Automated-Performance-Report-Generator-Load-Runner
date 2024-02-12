e40f0c52cec4326e129a66370c1c16114cadcd09
Masabathula V S Raghavendra Rao <masabathularao@uptycs.com>
2024-02-12 18:38:30 +0530

    fix

--- scripts/pg_badger.py
+++ scripts/pg_badger.py
@@ -42,7 +42,7 @@ def get_links(elastic_url,start_time_ist_str, end_time_ist_str):
                 report_link=f'http://{elastic_url}:5602/reports/view?file=/data/ondemand_reports/{report_name}/postgres.html'
 
                 print(report_link)   
-                return_links.append(report_link)
+                return_links[db]=report_link
             else:
                 print(f"Error generating report for db {db}: {response.status_code}")
     else:
@@ -51,56 +51,58 @@ def get_links(elastic_url,start_time_ist_str, end_time_ist_str):
 
 
 def take_screenshots_and_save(report_links,BASE_PGBADGER_IMAGES_PATH):
+    return_res={}
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--window-size=9999,9999')
     driver = webdriver.Chrome(options=chrome_options)
-    report_link=report_links[0]
-    driver.get(report_link)
-    return_res={}
-    partial_id = "menu"
-    elements = driver.find_elements(by=By.XPATH, value=f"//ul[@class='nav navbar-nav']//li[contains(@id, '{partial_id}')]")
-    stats_dict = {"Global Stats": "global-stats", "SQL Traffic": "sql-traffic", "Select Traffic": "select-traffic", "Write Traffic": "write-traffic", "Queries duration": "duration-traffic", "Prepared queries ratio": "prepared-queries-ratio", "General Activity": "general-activity", "Established connections": "established-connections", "Connections per database": "connections-per-database", "Connections per user": "connections-per-user", "Connections per host": "connections-per-host", "Simultaneous sessions": "simultaneous-sessions", "Histogram of sessions times": "histogram-session-times", "Sessions per database": "sessions-per-database", "Sessions per user": "sessions-per-user", "Sessions per host": "sessions-per-host", "Sessions per application": "sessions-per-app", "Checkpoints buffers": "checkpoints-buffers", "Checkpoints files": "checkpoints-files", "Checkpoints distance": "checkpoints-distance", "Checkpoint activity": "checkpoint-activity", "Checkpoint causes": "checkpoints-cause", "Size of temporary files": "tempfiles-size", "Number of temporary files": "tempfiles-number", "Temporary files activity": "tempfiles-activity", "Vacuums distribution": "vacuums-count", "Vacuums activity": "vacuums-activity", "Analyzes per Tables": "analyzes-per-table", "Vacuums per Tables": "vacuums-per-table", "Tuples removed": "tuples-removed-per-table", "Page removed": "pages-removed-per-table", "Locks by type": "locks-type", "Most frequent waiting queries (N)": "queries-most-frequent-waiting", "Queries that waited the most": "queries-that-waited-most", "Queries by type": "queries-by-type", "Queries by database": "queries-by-database", "Queries by user": "queries-by-user", "Duration by user": "duration-by-user", "Queries by host": "queries-by-host", "Queries by application": "queries-by-application", "Number of cancelled queries": "queries-cancelled-number", "Histogram of query times": "histogram-query-times", "Slowest individual queries": "slowest-individual-queries", "Time Consuming queries (N)": "time-consuming-queries", "Most frequent queries (N)": "most-frequent-queries", "Normalized slowest queries": "normalized-slowest-queries", "Time consuming prepare": "time-consuming-prepare", "Time consuming bind": "time-consuming-bind", "Log levels": "log-levels", "Events distribution": "minutes-errors-levels", "Most frequent errors/events (keys)": "most-frequent-errors-events"}
-    for element in elements:
-        try:
-            ul_element = WebDriverWait(element, 10).until(
-                EC.presence_of_element_located((By.XPATH, ".//ul[@class='dropdown-menu'][li/a[@href]]"))
-            )
-            element.click()
+    for db,report_link in report_links:
+        driver.get(report_link)
+        partial_id = "menu"
+        elements = driver.find_elements(by=By.XPATH, value=f"//ul[@class='nav navbar-nav']//li[contains(@id, '{partial_id}')]")
+        stats_dict = {"Global Stats": "global-stats", "SQL Traffic": "sql-traffic", "Select Traffic": "select-traffic", "Write Traffic": "write-traffic", "Queries duration": "duration-traffic", "Prepared queries ratio": "prepared-queries-ratio", "General Activity": "general-activity", "Established connections": "established-connections", "Connections per database": "connections-per-database", "Connections per user": "connections-per-user", "Connections per host": "connections-per-host", "Simultaneous sessions": "simultaneous-sessions", "Histogram of sessions times": "histogram-session-times", "Sessions per database": "sessions-per-database", "Sessions per user": "sessions-per-user", "Sessions per host": "sessions-per-host", "Sessions per application": "sessions-per-app", "Checkpoints buffers": "checkpoints-buffers", "Checkpoints files": "checkpoints-files", "Checkpoints distance": "checkpoints-distance", "Checkpoint activity": "checkpoint-activity", "Checkpoint causes": "checkpoints-cause", "Size of temporary files": "tempfiles-size", "Number of temporary files": "tempfiles-number", "Temporary files activity": "tempfiles-activity", "Vacuums distribution": "vacuums-count", "Vacuums activity": "vacuums-activity", "Analyzes per Tables": "analyzes-per-table", "Vacuums per Tables": "vacuums-per-table", "Tuples removed": "tuples-removed-per-table", "Page removed": "pages-removed-per-table", "Locks by type": "locks-type", "Most frequent waiting queries (N)": "queries-most-frequent-waiting", "Queries that waited the most": "queries-that-waited-most", "Queries by type": "queries-by-type", "Queries by database": "queries-by-database", "Queries by user": "queries-by-user", "Duration by user": "duration-by-user", "Queries by host": "queries-by-host", "Queries by application": "queries-by-application", "Number of cancelled queries": "queries-cancelled-number", "Histogram of query times": "histogram-query-times", "Slowest individual queries": "slowest-individual-queries", "Time Consuming queries (N)": "time-consuming-queries", "Most frequent queries (N)": "most-frequent-queries", "Normalized slowest queries": "normalized-slowest-queries", "Time consuming prepare": "time-consuming-prepare", "Time consuming bind": "time-consuming-bind", "Log levels": "log-levels", "Events distribution": "minutes-errors-levels", "Most frequent errors/events (keys)": "most-frequent-errors-events"}
+        for element in elements:
+            try:
+                ul_element = WebDriverWait(element, 10).until(
+                    EC.presence_of_element_located((By.XPATH, ".//ul[@class='dropdown-menu'][li/a[@href]]"))
+                )
+                element.click()
 
-            dropdown_options = ul_element.find_elements(By.TAG_NAME, 'li')
-            option_texts = [option.text for option in dropdown_options if option.text != ""]
-            
-            for option_text in option_texts:
-                print("Extracting details for : " , option_text)
-                option = ul_element.find_element(By.XPATH, f"./li[a[text()='{option_text}']]")
-                try:
-                    option.click()
-                except:
-                    print("this block is interactive")
-                sleep(2)
-
-                if option_text in stats_dict:
-                    id = stats_dict[option_text]
+                dropdown_options = ul_element.find_elements(By.TAG_NAME, 'li')
+                option_texts = [option.text for option in dropdown_options if option.text != ""]
+                
+                for option_text in option_texts:
+                    print("Extracting details for : " , option_text)
+                    option = ul_element.find_element(By.XPATH, f"./li[a[text()='{option_text}']]")
                     try:
-                        element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, id)))
-                        # Additional strategies for headless mode
-                        driver.execute_script("arguments[0].scrollIntoView(true);", element)
-                        actions = ActionChains(driver)
-                        actions.move_to_element(element).click().perform()
-                    except Exception as e:
-                        print(f"Error clicking element: {e}")
-                        element = driver.find_element_by_id(id)
-                    screenshot_path = f'{BASE_PGBADGER_IMAGES_PATH}/{id}.png'
-                    return_res[id]={}
-                    # print(f"Saving '{option_text}' details to path {screenshot_path}")
-                    element.screenshot(screenshot_path)
-                else:
-                    print("not found", option_text)
+                        option.click()
+                    except:
+                        print("this block is interactive")
+                    sleep(2)
 
-        except TimeoutException:
-            print("Dropdown menu not found within the specified time.")
-    driver.quit()
+                    if option_text in stats_dict:
+                        id = stats_dict[option_text]
+                        try:
+                            element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, id)))
+                            # Additional strategies for headless mode
+                            driver.execute_script("arguments[0].scrollIntoView(true);", element)
+                            actions = ActionChains(driver)
+                            actions.move_to_element(element).click().perform()
+                        except Exception as e:
+                            print(f"Error clicking element: {e}")
+                            element = driver.find_element_by_id(id)
+                        sleep(20)
+                        screenshot_path = f'{BASE_PGBADGER_IMAGES_PATH}/{id}_{db}.png'
+                        return_res[id]={}
+                        # print(f"Saving '{option_text}' details to path {screenshot_path}")
+                        element.screenshot(screenshot_path)
+                    else:
+                        print("not found", option_text)
+
+            except TimeoutException:
+                print("Dropdown menu not found within the specified time.")
+        driver.quit()
+
     return return_res
 
 def return_pgbadger_results(start_time_utc,end_time_utc,elastic_url,images_path):
@@ -108,13 +110,13 @@ def return_pgbadger_results(start_time_utc,end_time_utc,elastic_url,images_path)
 
     start_time = start_time_utc - timedelta(minutes=10)
     start_time = start_time.strftime(format_data)
-    end_time = end_time_utc + timedelta(hours=1) + timedelta(minutes=20)
+    end_time = end_time_utc + timedelta(minutes=10) + timedelta(hours=1)
     end_time = end_time.strftime(format_data)
 
     print("Converted start time UTC string is : " , start_time)
     print("Converted end time UTC string is : " , end_time)
-    links_list=get_links(elastic_url , start_time, end_time)
-    res=take_screenshots_and_save(links_list,images_path)
+    links=get_links(elastic_url , start_time, end_time)
+    res=take_screenshots_and_save(links,images_path)
     return res
 
    
