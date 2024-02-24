7748f183b358c0651709a94dc0473365895c2cb2
Masabathula V S Raghavendra Rao <masabathularao@uptycs.com>
2024-02-24 19:22:52 +0530

    changing charts look and feel

--- scripts/create_piechart_for_analysis.py
+++ scripts/create_piechart_for_analysis.py
@@ -16,9 +16,9 @@ kwargs={'startangle':270,
         'pctdistance':0.5
         }
 
-figsize=(25, 13)
+figsize=(27, 14)
 title_fontsize=18
-show_top_n=50
+show_top_n=10
 
 def compare_dfs(main,prev,merge_on):
     main.rename(columns={'avg': 'avg_main'}, inplace=True)
@@ -48,40 +48,42 @@ def create_piechart(mem_or_cpu,app_df,cont_df,nodetype):
     else:
         unit="cores"
     increased = app_df[app_df["absolute"] > 0][["application","absolute"]]
-    sum_app_increased = sum(increased["absolute"])
+    sum_app_increased = round(sum(increased["absolute"]),2)
     increased=increased.head(show_top_n)
     decreased = app_df[app_df["absolute"] < 0][["application","absolute"]]
-    sum_app_decreased = sum(decreased["absolute"])
+    sum_app_decreased = round(sum(decreased["absolute"]),2)
     decreased=decreased.tail(show_top_n).sort_values(by="absolute",ascending=True)
     decreased["absolute"] = decreased["absolute"].abs()
 
     fig, axs = plt.subplots(2, 2, figsize=figsize)  # 2 row, 2 columns
 
     axs[0][0].pie(increased['absolute'], labels=increased["application"],autopct=autopct_format(increased['absolute'],mem_or_cpu), **kwargs)
-    title_increased = f'applications contributing to {"increase".upper()} in {mem_or_cpu} usage for "{nodetype}" nodetype'
+    title_increased = f'applications contributing to {"increase".upper()} in {mem_or_cpu} usage for "{nodetype}" nodetype (+{sum_app_increased} {unit} ↑)'
     axs[0][0].set_title(title_increased, fontsize=title_fontsize)
 
     axs[0][1].pie(decreased['absolute'], labels=decreased["application"],autopct=autopct_format(decreased['absolute'],mem_or_cpu,'-'), **kwargs)
-    title_decreased = f'applications contributing to {"decrease".upper()} in {mem_or_cpu} usage for "{nodetype}" nodetype'
+    title_decreased = f'applications contributing to {"decrease".upper()} in {mem_or_cpu} usage for "{nodetype}" nodetype ({sum_app_decreased} {unit} ↓)'
     axs[0][1].set_title(title_decreased, fontsize=title_fontsize)
 
 #-----
     
     cont_increased = cont_df[cont_df["absolute"] > 0][["container","absolute"]]
-    sum_cont_increased = sum(cont_increased["absolute"])
+    sum_cont_increased = round(sum(cont_increased["absolute"]),2)
     cont_increased=cont_increased.head(show_top_n)
     cont_decreased = cont_df[cont_df["absolute"] < 0][["container","absolute"]]
-    sum_cont_decreased = sum(cont_decreased["absolute"])
+    sum_cont_decreased = round(sum(cont_decreased["absolute"]),2)
     cont_decreased=cont_decreased.tail(show_top_n).sort_values(by="absolute",ascending=True)
     cont_decreased["absolute"] = cont_decreased["absolute"].abs()
 
     axs[1][0].pie(cont_increased['absolute'], labels=cont_increased["container"],autopct=autopct_format(cont_increased['absolute'],mem_or_cpu), **kwargs)
-    cont_title_increased = f'containers contributing to {"increase".upper()} in {mem_or_cpu} usage for "{nodetype}" nodetype'
+    cont_title_increased = f'containers contributing to {"increase".upper()} in {mem_or_cpu} usage for "{nodetype}" nodetype (+{sum_cont_increased} {unit} ↑)'
     axs[1][0].set_title(cont_title_increased, fontsize=title_fontsize)
     
     axs[1][1].pie(cont_decreased['absolute'], labels=cont_decreased["container"],autopct=autopct_format(cont_decreased['absolute'],mem_or_cpu,'-'), **kwargs)
-    cont_title_decreased = f'containers contributing to {"decrease".upper()} in {mem_or_cpu} usage for "{nodetype}" nodetype'
+    cont_title_decreased = f'containers contributing to {"decrease".upper()} in {mem_or_cpu} usage for "{nodetype}" nodetype ({sum_cont_decreased} {unit} ↓)'
     axs[1][1].set_title(cont_title_decreased, fontsize=title_fontsize)
+
+    fig.suptitle(f"Complete {mem_or_cpu} analysis for '{nodetype}' nodetype\n\n", fontsize=title_fontsize+2)
 
     plt.gcf().set_facecolor(outer_background_color)
     # plt.subplots_adjust(left=10.1, right=10.9, bottom=0.1, top=0.9, wspace=70.4, hspace=700.4)
