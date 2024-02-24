73ff657568df68ca6e1da975e73f7bb2b4b83a63
Masabathula V S Raghavendra Rao <masabathularao@uptycs.com>
2024-02-24 18:51:34 +0530

    changing charts look and feel

--- scripts/create_piechart_for_analysis.py
+++ scripts/create_piechart_for_analysis.py
@@ -9,13 +9,16 @@ sns.set(rc={"text.color": text_color})
 
 kwargs={'startangle':270, 
         'wedgeprops': {'edgecolor': 'black', 'linewidth': 1.0},  # Set edge width
-        'textprops': {'fontsize': 12},  # Increase font size of labels
-        'colors' : ['#196F3D', '#873600', '#76448A', '#21618C', '#9C640C', '#717D7E']
+        'textprops': {'fontsize': 11},  # Increase font size of labels
+        'colors' : ['#196F3D', '#873600', '#76448A', '#21618C', '#9C640C', '#717D7E'],
+        'rotatelabels':True,
+        'labeldistance':0.8,
+        'pctdistance':0.5
         }
 
 figsize=(25, 13)
-title_fontsize=22
-show_top_n=10
+title_fontsize=18
+show_top_n=50
 
 def compare_dfs(main,prev,merge_on):
     main.rename(columns={'avg': 'avg_main'}, inplace=True)
@@ -28,7 +31,7 @@ def compare_dfs(main,prev,merge_on):
     merged_df=merged_df.sort_values(by='absolute', ascending=False)
     return merged_df
 
-def autopct_format(values,mem_or_cpu):
+def autopct_format(values,mem_or_cpu,sign='+'):
     if mem_or_cpu=="memory":
         unit="GB"
     else:
@@ -36,41 +39,52 @@ def autopct_format(values,mem_or_cpu):
     def my_format(pct):
         total = sum(values)
         val = round(float(pct*total/100),2)
-        return '{:.1f}%\n({} {})'.format(pct, val,unit)
+        return '{:.1f}%\n({}{} {})'.format(pct,sign, val,unit)
     return my_format
 
 def create_piechart(mem_or_cpu,app_df,cont_df,nodetype):
-    increased = app_df[app_df["absolute"] > 0][["application","absolute"]].head(show_top_n)
-    decreased = app_df[app_df["absolute"] < 0][["application","absolute"]].tail(show_top_n).sort_values(by="absolute",ascending=True)
+    if mem_or_cpu=="memory":
+        unit="GB"
+    else:
+        unit="cores"
+    increased = app_df[app_df["absolute"] > 0][["application","absolute"]]
+    sum_app_increased = sum(increased["absolute"])
+    increased=increased.head(show_top_n)
+    decreased = app_df[app_df["absolute"] < 0][["application","absolute"]]
+    sum_app_decreased = sum(decreased["absolute"])
+    decreased=decreased.tail(show_top_n).sort_values(by="absolute",ascending=True)
     decreased["absolute"] = decreased["absolute"].abs()
 
     fig, axs = plt.subplots(2, 2, figsize=figsize)  # 2 row, 2 columns
 
     axs[0][0].pie(increased['absolute'], labels=increased["application"],autopct=autopct_format(increased['absolute'],mem_or_cpu), **kwargs)
-    title_increased = f'applications contributing to increase in {mem_or_cpu} usage for {nodetype} nodetype'.capitalize()
+    title_increased = f'applications contributing to {"increase".upper()} in {mem_or_cpu} usage for "{nodetype}" nodetype'
     axs[0][0].set_title(title_increased, fontsize=title_fontsize)
 
-    axs[0][1].pie(decreased['absolute'], labels=decreased["application"],autopct=autopct_format(decreased['absolute'],mem_or_cpu), **kwargs)
-    title_decreased = f'applications contributing to decrease in {mem_or_cpu} usage for {nodetype} nodetype'.capitalize()
+    axs[0][1].pie(decreased['absolute'], labels=decreased["application"],autopct=autopct_format(decreased['absolute'],mem_or_cpu,'-'), **kwargs)
+    title_decreased = f'applications contributing to {"decrease".upper()} in {mem_or_cpu} usage for "{nodetype}" nodetype'
     axs[0][1].set_title(title_decreased, fontsize=title_fontsize)
 
 #-----
     
-    cont_increased = cont_df[cont_df["absolute"] > 0][["container","absolute"]].head(show_top_n)
-    cont_decreased = cont_df[cont_df["absolute"] < 0][["container","absolute"]].tail(show_top_n).sort_values(by="absolute",ascending=True)
+    cont_increased = cont_df[cont_df["absolute"] > 0][["container","absolute"]]
+    sum_cont_increased = sum(cont_increased["absolute"])
+    cont_increased=cont_increased.head(show_top_n)
+    cont_decreased = cont_df[cont_df["absolute"] < 0][["container","absolute"]]
+    sum_cont_decreased = sum(cont_decreased["absolute"])
+    cont_decreased=cont_decreased.tail(show_top_n).sort_values(by="absolute",ascending=True)
     cont_decreased["absolute"] = cont_decreased["absolute"].abs()
 
     axs[1][0].pie(cont_increased['absolute'], labels=cont_increased["container"],autopct=autopct_format(cont_increased['absolute'],mem_or_cpu), **kwargs)
-    cont_title_increased = f'containers contributing to increase in {mem_or_cpu} usage for {nodetype} nodetype'.capitalize()
+    cont_title_increased = f'containers contributing to {"increase".upper()} in {mem_or_cpu} usage for "{nodetype}" nodetype'
     axs[1][0].set_title(cont_title_increased, fontsize=title_fontsize)
     
-    axs[1][1].pie(cont_decreased['absolute'], labels=cont_decreased["container"],autopct=autopct_format(cont_decreased['absolute'],mem_or_cpu), **kwargs)
-    cont_title_decreased = f'containers contributing to decrease in {mem_or_cpu} usage for {nodetype} nodetype'.capitalize()
+    axs[1][1].pie(cont_decreased['absolute'], labels=cont_decreased["container"],autopct=autopct_format(cont_decreased['absolute'],mem_or_cpu,'-'), **kwargs)
+    cont_title_decreased = f'containers contributing to {"decrease".upper()} in {mem_or_cpu} usage for "{nodetype}" nodetype'
     axs[1][1].set_title(cont_title_decreased, fontsize=title_fontsize)
-               
-    plt.gcf().set_facecolor(outer_background_color)
-    # plt.subplots_adjust(wspace=0.5) 
 
+    plt.gcf().set_facecolor(outer_background_color)
+    # plt.subplots_adjust(left=10.1, right=10.9, bottom=0.1, top=0.9, wspace=70.4, hspace=700.4)
     plt.tight_layout()
     plt.savefig(f"/Users/masabathulararao/Documents/Loadtest/save-report-data-to-mongo/scripts/csv/{mem_or_cpu}_{nodetype}.png")
 
@@ -79,6 +93,8 @@ def create_piechart(mem_or_cpu,app_df,cont_df,nodetype):
 
 def compress(val):
     last = val.split('/')[-1]
+    if len(last)>30:
+        last = last.split('-')[-1]
     last=last.capitalize()
     return last
 
