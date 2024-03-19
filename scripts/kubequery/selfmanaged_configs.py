ac1f468daa59e8cc9ffabd7483f4fdb5030cff74
Madhusudhan Reddy <mreddy@uptycs.com>
2024-03-19 09:34:14 +0530

    remove support for dns_lookup_events table from kspm and sm loads and add support for osquery-state-manager chart

--- scripts/kubequery/selfmanaged_configs.py
+++ scripts/kubequery/selfmanaged_configs.py
@@ -3,7 +3,7 @@ from pathlib import Path
 #json_directory = ""
 PROJECT_ROOT = Path(__file__).resolve().parent
 
-tables = ['vulnerabilities_scanned_images', 'vulnerabilities','process_events','socket_events', 'process_file_events','dns_lookup_events','containers','docker_images','crio_images']
+tables = ['vulnerabilities_scanned_images', 'vulnerabilities','process_events','socket_events', 'process_file_events','containers','docker_images','crio_images']
 
 
 vsi_data = {"VulnerabilitiesScannedImages_Count": 0, 
@@ -12,7 +12,6 @@ vsi_data = {"VulnerabilitiesScannedImages_Count": 0,
              "ProcessEvents_Count": 0, 
              "SocketEvents_Count": 0, 
              "ProcessFileEvents_Count": 0, 
-             "DnsLookupEvents_Count": 0, 
              "Containers_Count": 0,
              "DockerImages_Count": 0,
              "CrioImages_Count": 0}
@@ -20,7 +19,7 @@ vsi_data = {"VulnerabilitiesScannedImages_Count": 0,
 asset_count = 200
 # sim_nodes = ['s13sim1','s13sim2','s13sim3','s13sim4']
 
-ports = [28001 ,28002]
+ports = [28001]
 # In Minutes
 deltaTime = 60 
 
@@ -31,7 +30,6 @@ key_mapping = {
     'ProcessEvents_Count': 'process_events',
     'SocketEvents_Count': 'socket_events',
     'ProcessFileEvents_Count': 'process_file_events',
-    'DnsLookupEvents_Count': 'dns_lookup_events',
     'Containers_Count': 'containers',
     "DockerImages_Count":"docker_images",
     "CrioImages_Count":"crio_images"}
