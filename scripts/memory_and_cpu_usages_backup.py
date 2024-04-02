753908ae7390ec08767e52358ab3884b3146a572
Masabathula V S Raghavendra Rao <masabathularao@uptycs.com>
2024-04-02 12:17:24 +0530

    decouple test_env_file from resource calculation

--- scripts/memory_and_cpu_usages_backup.py
+++ scripts/memory_and_cpu_usages_backup.py
@@ -1,5 +1,4 @@
-from collections import defaultdict
-from helper import execute_prometheus_query,execute_point_prometheus_query
+import requests
 import json
 
 HOST = 'Host'
@@ -8,37 +7,21 @@ cpu_tag = "CPU"
 memory_unit = "GB"
 cpu_unit = "cores"
 
+# node_level_usage_app_names={
+#             "sum":[ ".*osqLogger.*", "kafka",".*ruleEngine.*","tls", "trino" , "osqueryIngestion"],
+#             "avg":[]
+#           }
+
 class MC_comparisions:
-    def __init__(self,prom_con_obj,start_timestamp,end_timestamp,hours,include_nodetypes):
+    def __init__(self,prom_con_obj,start_timestamp,end_timestamp):
         self.curr_ist_start_time=start_timestamp
         self.curr_ist_end_time=end_timestamp
         self.prom_con_obj=prom_con_obj
-        self.hours=hours
-        self.include_nodetypes=include_nodetypes
-
-        total_memory_capacity_query = "sum(uptycs_total_memory/(1024*1024)) by (node_type,host_name)"
-        memory_result = execute_point_prometheus_query(self.prom_con_obj,self.curr_ist_start_time,total_memory_capacity_query)
-        self.node_ram_capacity = {}
-        self.all_node_types_mapping=defaultdict(lambda:[])
-
-        for line in memory_result:
-            node_name = line["metric"]["host_name"]
-            capacity = float(line["value"][1])
-            self.node_ram_capacity[node_name] = capacity  
-            self.all_node_types_mapping[line["metric"]["node_type"]].append(node_name)
-     
-        total_cpu_capacity_query = "sum(uptycs_loadavg_cpu_info) by (host_name,cpu_processor)"
-        cpu_result = execute_point_prometheus_query(self.prom_con_obj,self.curr_ist_start_time,total_cpu_capacity_query)
-        self.node_cores_capacity = {}
-        for line in cpu_result:
-            node_name = line["metric"]["host_name"]
-            cpu_capacity = float(line["metric"]["cpu_processor"]) + 1 
-            self.node_cores_capacity[node_name] = cpu_capacity
-        
-        self.all_node_types=list(self.all_node_types_mapping.keys())
-        print("All nodetypes found are : " , self.all_node_types)
-        print("Memory capacity : \n" , json.dumps(self.node_ram_capacity,indent=4))
-        print("CPU capacity : \n" , json.dumps(self.node_cores_capacity,indent=4))
+        self.PROMETHEUS = self.prom_con_obj.prometheus_path
+        self.API_PATH = self.prom_con_obj.prom_api_path
+        self.test_env_file_path=prom_con_obj.test_env_file_path
+        with open(self.test_env_file_path, 'r') as file:
+            self.nodes_data = json.load(file)
 
     def extract_data(self,queries,tag,unit):
         final=dict()
@@ -46,40 +29,61 @@ class MC_comparisions:
 
         for query in queries:
             final[query] = {}
-            print(f"-------processing {tag} for {query} (timestamp : {self.curr_ist_start_time} to {self.curr_ist_end_time})")
-            for res in execute_prometheus_query(self.prom_con_obj,self.curr_ist_start_time,self.curr_ist_end_time,queries[query],self.hours):
-                hostname = res['metric']['host_name'] 
-                avg = res["values"]["average"]
-                minimum = res["values"]["minimum"]
-                maximum = res["values"]["maximum"]
+            PARAMS = {
+                'query': queries[query],
+                'start': self.curr_ist_start_time,
+                'end': self.curr_ist_end_time,
+                'step':60
+            }
+            response = requests.get(self.PROMETHEUS + self.API_PATH, params=PARAMS)
+            print(f"-------processing {tag} for {query} (timestamp : {self.curr_ist_start_time} to {self.curr_ist_end_time}), Status code : {response.status_code}")
+            if response.status_code != 200:print("ERROR : Request failed")
+            result = response.json()['data']['result']
+            if query==HOST:
+                print("All hosts : ", [r['metric']['host_name'] for r in result])
+            for res in result:
+                hostname = res['metric']['host_name']
+                # print(f"Processing node-level {tag} usage for {query} : {hostname}")
+                if str(hostname).endswith('v') and len(hostname)>1:
+                    hostname = str(hostname)[:-1]
+                values = [float(i[1]) for i in res['values']]   
+                avg = sum(values) / len(values)
+                minimum = min(values)
+                maximum = max(values)
                 final[query][hostname] = {"percentage":{"average":avg , "minimum":minimum , "maximum":maximum}}
                 final[query][hostname][unit]={}
-                if tag == memory_tag:
-                    final[query][hostname][unit]={
-                        'average':avg * float(self.node_ram_capacity[hostname]) / 100,
-                        'minimum':minimum * float(self.node_ram_capacity[hostname]) / 100,
-                        'maximum':maximum * float(self.node_ram_capacity[hostname]) / 100
-                    }
-                else:
-                    if query == HOST:
+                try:
+                    if tag == memory_tag:
                         final[query][hostname][unit]={
-                            'average':avg * float(self.node_cores_capacity[hostname]) / 100,
-                            'minimum':minimum * float(self.node_cores_capacity[hostname]) / 100,
-                            'maximum':maximum * float(self.node_cores_capacity[hostname]) / 100
+                            'average':avg * float(self.nodes_data[hostname]['ram']) / 100,
+                            'minimum':minimum * float(self.nodes_data[hostname]['ram']) / 100,
+                            'maximum':maximum * float(self.nodes_data[hostname]['ram']) / 100
                         }
                     else:
-                        final[query][hostname][unit]={
-                            'average':avg/100,
-                            'minimum': minimum/100,
-                            'maximum': maximum/100
-                        }
-                
+                        if query == HOST:
+                            final[query][hostname][unit]={
+                                'average':avg * float(self.nodes_data[hostname]['cores']) / 100,
+                                'minimum':minimum * float(self.nodes_data[hostname]['cores']) / 100,
+                                'maximum':maximum * float(self.nodes_data[hostname]['cores']) / 100
+                            }
+                        else:
+                            final[query][hostname][unit]={
+                                'average':avg/100,
+                                'minimum': minimum/100,
+                                'maximum': maximum/100
+                            }
+                except Exception as e:
+                    if hostname in self.nodes_data['other_nodes']:
+                        print(f"WARNING : KEY ERROR : Could not fetch details for host {hostname}, please make sure you have details for {hostname} correctly configured in stack's json file. {e}")
+                    else:
+                        raise RuntimeError(F"KEY ERROR : Could not fetch details for host {hostname}, please make sure you have details for {hostname} correctly configured in stack's json file. {e}") from e
+
         #calculate overall pnodes,dnodes,pgnodes usage
         new_data = final[HOST]
-        for node_type in self.include_nodetypes:
+        for node_type in ["pnodes" , "dnodes" , "pgnodes","stsnodes"]:
             print(f"Calculating overall {tag} usages for node-type : {node_type}")
             new_sum=0
-            for node in self.all_node_types_mapping[node_type]:
+            for node in self.nodes_data[node_type]:
                 try:
                     new_sum+=new_data[node][unit]["average"]
                 except KeyError as e:
@@ -94,26 +98,50 @@ class MC_comparisions:
         final=dict()
         for query in queries:
             final[query] = {}
-            print(f"----------processing {tag} for {query} (timestamp : {self.curr_ist_start_time} to {self.curr_ist_end_time})")
-            for res in execute_prometheus_query(self.prom_con_obj,self.curr_ist_start_time,self.curr_ist_end_time,queries[query],self.hours):
+            PARAMS = {
+                'query': queries[query],
+                'start': self.curr_ist_start_time,
+                'end': self.curr_ist_end_time,
+                'step':60
+            }
+            response = requests.get(self.PROMETHEUS + self.API_PATH, params=PARAMS)
+            print(f"----------processing {tag} for {query} (timestamp : {self.curr_ist_start_time} to {self.curr_ist_end_time}), Status code : {response.status_code}")
+            if response.status_code != 200:print("ERROR : Request failed")
+            result = response.json()['data']['result']
+            for res in result:
                 container_name = res['metric']['container_name']
-                avg = res["values"]["average"]
-                minimum = res["values"]["minimum"]
-                maximum = res["values"]["maximum"]
+                # print(f"Processing container-level {tag} usage for {query} : {container_name}")
+                values = [float(i[1]) for i in res['values']]   
+                avg = sum(values) / len(values)
+                minimum = min(values)
+                maximum = max(values)
+                if tag == cpu_tag:
+                    avg = avg/100
+                    minimum = minimum/100
+                    maximum = maximum/100
                 final[query][container_name] = {f"{unit}":{"average":avg , "minimum":minimum , "maximum":maximum}}
         return final 
     def extract_app_data(self,queries,tag,unit):
         final=dict()
         print(f"----------processing application level {tag} usages for (timestamp : {self.curr_ist_start_time} to {self.curr_ist_end_time})")
         for query in queries:
-            print(f"processing {tag} usage for {query} application (timestamp : {self.curr_ist_start_time} to {self.curr_ist_end_time})")
-            result=execute_prometheus_query(self.prom_con_obj,self.curr_ist_start_time,self.curr_ist_end_time,queries[query],self.hours)
+            PARAMS = {
+                'query': queries[query],
+                'start': self.curr_ist_start_time,
+                'end': self.curr_ist_end_time,
+                'step':60
+            }
+            response = requests.get(self.PROMETHEUS + self.API_PATH, params=PARAMS)
+            print(f"processing {tag} usage for {query} application (timestamp : {self.curr_ist_start_time} to {self.curr_ist_end_time}), Status code : {response.status_code}")
+            if response.status_code != 200:print("ERROR : Request failed")
+            result = response.json()['data']['result']
             if len(result)==0:
                 print(f"WARNING : No data found for : {query}, the query executed is : {queries[query]}")
                 continue
-            avg = result[0]["values"]["average"]
-            minimum = result[0]["values"]["minimum"]
-            maximum = result[0]["values"]["maximum"]
+            values = [float(i[1]) for i in result[0]['values']]
+            avg = sum(values) / len(values)
+            minimum = min(values)
+            maximum = max(values)
             try:
                 print(str(query)==str(result[0]['metric']['app_name']))
             except Exception as e:
@@ -131,7 +159,7 @@ class MC_comparisions:
         cpu_queries.update(dict([(app,f"{key}(uptycs_app_cpu{{app_name=~'{app}'}}) by (host_name)") for key,app_list in app_names.items() for app in app_list]))
 
         container_memory_queries = {'container' : "sum(uptycs_docker_mem_used{}/(1000*1000*1000)) by (container_name)",}
-        container_cpu_queries = {'container' : "sum(uptycs_docker_cpu_stats{}/100) by (container_name)",}
+        container_cpu_queries = {'container' : "sum(uptycs_docker_cpu_stats{}) by (container_name)",}
 
         app_level_memory_queries = dict([(app,f"sum({key}(uptycs_app_memory{{app_name=~'{app}'}}) by (app_name))") for key,app_list in app_names.items() for app in app_list])
         app_level_cpu_queries = dict([(app,f"sum({key}(uptycs_app_cpu{{app_name=~'{app}'}}) by (app_name))") for key,app_list in app_names.items() for app in app_list])
