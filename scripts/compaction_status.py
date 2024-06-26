5258e4ce7e42e142d513f527bbd7656af25d56ba
pshravani-uptycs <pshravani@uptycs.com>
2024-06-26 18:39:28 +0530

    fixed error in compaction status

--- scripts/compaction_status.py
+++ scripts/compaction_status.py
@@ -1,14 +1,12 @@
 from elasticsearch import Elasticsearch
-import datetime
 from datetime import datetime
 
-
 class CompactionStatus:
-    def __init__(self,stack_obj,elastic_ip):
+    def __init__(self, stack_obj, elastic_ip):
         self.stack_obj = stack_obj
-        try:            
+        try:
             self.elasticsearch_host = f"http://{elastic_ip}:9200"
-            self.elastic_client = Elasticsearch(hosts=[self.elasticsearch_host], timeout=240)
+            self.elastic_client = Elasticsearch(hosts=[self.elasticsearch_host], request_timeout=240)
 
             dt_object = datetime.utcfromtimestamp(stack_obj.start_timestamp)
             self.formatted_starttime = dt_object.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
@@ -17,82 +15,82 @@ class CompactionStatus:
 
             self.index_name = "uptycs-*"
         except Exception as e:
-             self.stack_obj.log.error(f"An error occurred during initialization: {e}")
+            self.stack_obj.log.error(f"An error occurred during initialization: {e}")
 
     def build_query(self):
         try:
             query_body = {
                 "aggs": {
                     "0": {
-                    "date_histogram": {
-                        "field": "event.tags.upt_day.date_value",
-                        "calendar_interval": "1d",
-                        "time_zone": "Asia/Calcutta",
-                    },
-                    "aggs": {
-                        "Files Ingested": {
-                        "sum": {
-                            "field": "event.metrics.files_per_batch.int_value"
-                        }
+                        "date_histogram": {
+                            "field": "event.tags.upt_day.date_value",
+                            "calendar_interval": "1d",
+                            "time_zone": "Asia/Calcutta",
                         },
-                        "Files Compacted": {
-                        "sum": {
-                            "field": "event.metrics.uncompacted_file_count.int_value"
+                        "aggs": {
+                            "Files Ingested": {
+                                "sum": {
+                                    "field": "event.metrics.files_per_batch.int_value"
+                                }
+                            },
+                            "Files Compacted": {
+                                "sum": {
+                                    "field": "event.metrics.uncompacted_file_count.int_value"
+                                }
+                            },
+                            "Files Skipped": {
+                                "sum": {
+                                    "field": "event.metrics.skip_compaction_file_cnt.int_value"
+                                }
+                            },
+                            "Files Ready for Archival": {
+                                "sum": {
+                                    "field": "event.metrics.l1_compacted_file_count.int_value"
+                                }
+                            },
+                            "Files Archived": {
+                                "sum": {
+                                    "field": "event.metrics.total_archived_count.int_value"
+                                }
+                            }
                         }
-                        },
-                        "Files Skipped": {
-                        "sum": {
-                            "field": "event.metrics.skip_compaction_file_cnt.int_value"
-                        }
-                        },
-                        "Files Ready for Archival": {
-                        "sum": {
-                            "field": "event.metrics.l1_compacted_file_count.int_value"
-                        }
-                        },
-                        "Files Archived": {
-                        "sum": {
-                            "field": "event.metrics.total_archived_count.int_value"
-                        }
-                        }
-                    }
                     }
                 },
                 "size": 0,
                 "fields": [
                     {
-                    "field": "@timestamp",
-                    "format": "date_time"
+                        "field": "@timestamp",
+                        "format": "date_time"
                     },
                     {
-                    "field": "event.tags.upt_day.date_value",
-                    "format": "date_time"
+                        "field": "event.tags.upt_day.date_value",
+                        "format": "date_time"
                     },
                     {
-                    "field": "event.timestamp",
-                    "format": "date_time"
+                        "field": "event.timestamp",
+                        "format": "date_time"
                     }
                 ],
                 "query": {
                     "bool": {
-                    "filter": [
-                        {
-                        "range": {
-                            "event.tags.upt_day.date_value": {
-                            "format": "strict_date_optional_time",
-                            "gte": self.formatted_starttime,
-                            "lte": self.formatted_endtime
+                        "filter": [
+                            {
+                                "range": {
+                                    "event.tags.upt_day.date_value": {
+                                        "format": "strict_date_optional_time",
+                                        "gte": self.formatted_starttime,
+                                        "lte": self.formatted_endtime
+                                    }
+                                }
                             }
-                        }
-                        }
-                    ],
+                        ],
                     }
                 }
-                }
+            }
 
             return query_body
         except Exception as e:
-             self.stack_obj.log.error(f"An error occurred while constructing the query: {e}")
+            self.stack_obj.log.error(f"An error occurred while constructing the query: {e}")
 
     def execute_query(self):
         try:
@@ -102,7 +100,6 @@ class CompactionStatus:
             result_dict = {}
 
             for bucket in aggregations.get('0', {}).get('buckets', []):
-               
                 date_string = bucket.get('key_as_string', '')
                 date_part = date_string.split('T')[0]
                 files_archived = bucket.get('Files Archived', {}).get('value', 0)
@@ -119,21 +116,20 @@ class CompactionStatus:
                 self.stack_obj.log.info(f"Files Compacted: {files_compacted}")
                 print()
 
+                if date_part not in result_dict:
+                    result_dict[date_part] = {}
+
                 result_dict[date_part]["Files Archived"] = files_archived
                 result_dict[date_part]["Files Skipped"] = files_skipped
                 result_dict[date_part]["Files Ingested"] = files_ingested
                 result_dict[date_part]["Files Ready for Archival"] = files_ready_for_archival
                 result_dict[date_part]["Files Compacted"] = files_compacted
 
-                return result_dict
+            return result_dict
         except Exception as e:
             self.stack_obj.log.error(f"An error occurred while executing the query: {e}")
 
-# # Example usage:
-# if __name__ == "__main__":
-#     obj = CompactionStatus()
-#     result = obj.execute_query()
-            
+# Example usage:
 if __name__ == "__main__":
     from settings import stack_configuration
     import json
@@ -142,10 +138,10 @@ if __name__ == "__main__":
         "load_duration_in_hrs":14,
         "test_env_file_name":'s1_nodes.json'
     }
-    stack_obj = stack_configuration(variables)    
+    stack_obj = stack_configuration(variables)
     stack_obj.log.info("******* Fetching Compaction Status details...")
-    with open(stack_obj.test_env_file_path , 'r') as file:
+    with open(stack_obj.test_env_file_path, 'r') as file:
         test_env_json_details = json.load(file)
-    compaction = CompactionStatus(stack_obj=stack_obj,elastic_ip=test_env_json_details['elastic'])
+    compaction = CompactionStatus(stack_obj=stack_obj, elastic_ip=test_env_json_details['elastic'])
     compaction_status = compaction.execute_query()
     print(compaction_status)
