cd8de101a2cedf6f4b7ac1c74008773c86f6e4a6
Masabathula V S Raghavendra Rao <masabathularao@uptycs.com>
2024-08-24 14:49:22 +0530

    beautify UI

--- scripts/app.py
+++ scripts/app.py
@@ -32,6 +32,25 @@ def process():
     collection_name = request.form['loadname']
 
 
+    # url='https://uptycsjira.atlassian.net'
+    # email_address = "masabathularao@uptycs.com"
+    # api_key = "ATATT3xFfGF02rG4e5JQzZZ_mVdAkwKKGnjRLYIupWToEGxZm8X-r5dUrAzSAdzGi5FPXMIn_IacnJjOwORsOQV7noObZmkdHqsaHHIzw4pTVyid2Jh3rVmLjM8iw5_hmaK7rFWSMz1JBpQq44vGV1FJs7P-89zijob43kBuxHzfFJJxl5IlM0w=7CE826E3"
+    # space = '~71202040c8bf45840d41c598c0efad54382c7b'
+
+
+    # url='https://raghav-m.atlassian.net'
+    # email_address = "pbpraghav@gmail.com"
+    # api_key = "ATATT3xFfGF0Tne5mgz28ho5MDsnw1LL_auMF9d0nSufjfGj98I_W2pfpMfbsL1v74wDDPAm5evj46IOmYBQGF8g9UNW8nrsuTur9TuOuKIGnRC2T17j6dFj1hmwOYuHTor9GvtrBNurI92gOBdPwqlgjottdh1Y3WqpfHn2LSrMRd3IDtgrLcc=29FDB196"
+    # space = '~712020a6f5183ca4bf41dcae421b10e977a0c1'
+    
+    # parent_page_title = 'TEST'  
+    # import uuid
+    # report_title = f"TEST {uuid.uuid4()}"
+
+    # list_of_sprint_runs_to_show_or_compare = [(160,5)]
+    # database_name = "Osquery_LoadTests_New"
+    # collection_name = "ControlPlane"
+
 
     obj = perf_load_report_publish(database_name, collection_name, list_of_sprint_runs_to_show_or_compare, parent_page_title, report_title, email_address, api_key, space, url)
     if "new_format" not in obj.all_keys:
